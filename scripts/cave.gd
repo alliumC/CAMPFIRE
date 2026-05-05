ec8268405ce9fe83a84a93d28d802de41eeeb639
Akio1997 <timothymcquin1997@gmail.com>
2026-05-05 20:55:39 +0800

    feat: redesign dungeon, fix lighting

--- scripts/cave.gd
+++ scripts/cave.gd
@@ -2,4 +2,4 @@ extends Area2D
 
 
 func _on_area_entered(_area: Area2D) -> void:
-	get_tree().change_scene_to_file("res://scenes/dungeon.tscn")
+	get_tree().call_deferred("change_scene_to_file", "uid://cd2ekv3lcnfgt")
