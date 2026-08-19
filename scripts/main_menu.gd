4440909f0ca7d8c91c01c08e1640048d6aa59c0f
Jhaz <jhazmineaudrey7@gmail.com>
2026-08-19 13:14:37 +0800

    fix: update main menu scene internals

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -13,7 +13,7 @@ func _ready():
 	lower_pitch_loop()
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/world.tscn")
+	get_tree().change_scene_to_file("uid://byk555wkqyfg6")
 
 func _on_options_pressed() -> void:
 	title.visible = false
